a0f4c5da722fe3a0e23a219b99a76ee361918fcc
m-dzn <88yangkh@gmail.com>
2022-05-20 17:13:40 +0900

    :recycle: Rename Design Token constant variable

--- styles/index.ts
+++ styles/index.ts
@@ -1,2 +1,3 @@
 export * from "./GlobalStyles";
 export * from "./themes";
+export * from "./tokens";
---
+++ styles/tokens.ts
@@ -0,0 +1,9 @@
+const FONT_SIZE = {
+  LARGE: 18,
+  normal: 16,
+  SMALL: 14,
+};
+
+export const TOKEN = {
+  FONT_SIZE,
+};
