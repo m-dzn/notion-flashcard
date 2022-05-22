6cedcaa67978a81928c6ab8d09d17373c4d71cd1
m-dzn <88yangkh@gmail.com>
2022-05-22 23:32:23 +0900

    :heavy_plus_sign: Add Recoil dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+import { RecoilRoot } from "recoil";
 
 import { defaultTheme, GlobalStyles } from "@/styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
